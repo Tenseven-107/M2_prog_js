4dd32764ad70c593de47fe21a07fc2be75ad1bb2
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2022-12-07 20:52:45 +0100

    some stuff idk

---
+++ loopsandarrays/app.js
@@ -0,0 +1,11 @@
+class App
+{
+    runApplication()
+    {
+        let arr = ["Carpenter Brut", "Babymetal", 
+                    "Hollywood burns", "Many more artists"]
+    }
+}
+
+let app = new App();
+app.runApplication();
