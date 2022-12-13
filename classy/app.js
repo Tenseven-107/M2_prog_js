8085cc976bf0454122debb26a7e79d2c85667596
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2022-12-13 11:33:29 +0100

    followed new lesson

---
+++ classy/app.js
@@ -0,0 +1,71 @@
+
+// Dino
+class Dino
+{
+    constructor(naam, vleeseter, leeftijd)
+    {
+        this.naam = naam;
+        this.vleeseter = vleeseter;
+        this.leeftijd = leeftijd;
+        this.leeft = true;
+        this.honger = true;
+    }
+
+    eetDino(d)
+    {
+        d.leeft = false;
+        this.honger = false;
+        console.log(this.naam, "eet de dino", d.naam)
+    }
+
+    eetPlant(plant)
+    {
+        this.honger = false;
+        console.log(this.naam, "eet de plant", plant.naam)
+    }
+}
+
+// Plant
+class Plant
+{
+    constructor(naam)
+    {
+        this.naam = naam;
+    }
+}
+
+
+
+// Run
+class App
+{
+    constructor(dinos = [], plants = [])
+    {
+        this.plants = plants;
+        this.dinos = dinos;
+    }
+
+    runApplication()
+    {
+        let grass = new Plant("Grass");
+        let bessen = new Plant("Bessen");
+
+        let trex = new Dino("Trex", true, 10);
+        let dodo = new Dino("Dodo", false, 5);
+        let brachiosaurus = new Dino("Brachiosaurus", false, 20);
+
+        this.plants.push(grass, bessen);
+        this.dinos.push(trex, dodo, brachiosaurus);
+
+        console.log(this.dinos, this.plants);
+
+        dodo.eetPlant(grass);
+        trex.eetDino(dodo);
+        brachiosaurus.eetPlant(bessen);
+
+        console.log(this.dinos, this.plants);
+    }
+}
+
+let app = new App();
+app.runApplication();
